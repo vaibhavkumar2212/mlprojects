62a84b252311b05e8fbda58a5a610a6f3beb2a14
Vaibhav Kumar <vaibhavkumar2212@gmail.com>
2023-09-08 23:53:37 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Vaibhav Kumar',
+author_email='vaibhavkumar2212@gmail.com',
+packages=find_packages()
+install_requires=['pandas','numpy','seaborn']
+
+
+)
